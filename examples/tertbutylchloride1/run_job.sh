37307218a07c5b5ac69a296f9b9eb19f3281b55f
Jeroen Engelberts <jeroene@int1.bullx>
2018-04-13 09:24:27 +0200

    Modified cartesius-demo into demokun also in the scripts

--- examples/tertbutylchloride1/run_job.sh
+++ examples/tertbutylchloride1/run_job.sh
@@ -5,8 +5,8 @@ if [ "x$1" == "x" ]; then
 fi
 BONDLENGTH=${1}
 MYTEMP=`mktemp -d ${TMPDIR}/tert.XXXX`
-MYHOME=${HOME}/cartesius-demo/examples/tertbutylchloride
+MYHOME=${HOME}/demokun/examples/tertbutylchloride
 cd ${MYTEMP}
 cat ${MYHOME}/template | sed "s/LENGTH/${BONDLENGTH}/g" > input_${BONDLENGTH}
-${HOME}/cartesius-demo/bin/gamess < input_${BONDLENGTH} > ${MYHOME}/output_${BONDLENGTH}
+${HOME}/demokun/bin/gamess < input_${BONDLENGTH} > ${MYHOME}/output_${BONDLENGTH}
 rm -rf ${MYTEMP}
