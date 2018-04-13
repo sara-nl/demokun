650950936784ab2fbb559f9bf70bdd1ecb74f86f
Jeroen Engelberts <jeroene@int2.bullx>
2018-04-13 09:44:11 +0200

    Fixed another final bug.

--- examples/tertbutylchloride1/submit_job.sh
+++ examples/tertbutylchloride1/submit_job.sh
@@ -2,7 +2,7 @@
 #SBATCH -t 10
 #SBATCH -n 1 
 #SBATCH --reservation=demokun
-for i in "1.6 1.8 1.9 2.0 2.1 2.2 2.5 3.0 5.0 10.0" ; do
+for i in 1.6 1.8 1.9 2.0 2.1 2.2 2.5 3.0 5.0 10.0;  do
    ./run_job.sh ${i} &
 done
 wait
